3fdcf0936bb5570f27b62313c1b241beade4ce1c
Zulham Azwar Achmad <zulham724@students.unnes.ac.id>
2021-04-15 14:49:49 +0700

    menambah halaman dashboard 50% done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,6 +6,8 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
+  // for devices that support a higher refresh rate for the input than the display itself.
+  // GestureBinding.instance.resamplingEnabled = true;
   runApp(
     GetMaterialApp(
       title: "Application",
