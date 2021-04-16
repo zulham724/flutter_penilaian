69d802d57f69d56fcff5daceca28ea71a2f4b25d
depa1999 <112201805960@mhs.dinus.ac.id>
2021-04-16 19:05:19 +0700

    profile page

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,19 +1,101 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/dashboard_controller.dart';
 
 class DashboardView extends GetView<DashboardController> {
+  List<int> exampleList = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'DashboardView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          toolbarHeight: 120, // Set this height
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Container(
+              child: Container(
+                  margin: new EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [Text('AGPAII'), Text('PENILAIAN')],
+                          ),
+                          Spacer(),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20)),
+                            onPressed: () => true,
+                            child: Text('Ranking'),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20)),
+                            onPressed: () => true,
+                            child: Text('Paket Soal'),
+                          ),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20)),
+                            child: Text('Butir Soal'),
+                          ),
+                          Spacer(),
+                          RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(20)),
+                            child: Text('Filter'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ))),
         ),
-      ),
-    );
+        body: Container(
+          margin: new EdgeInsets.all(20),
+          child: new ListView.builder(
+            itemCount: exampleList.length,
+            itemBuilder: (context, index) {
+              return new Card(
+                child: new Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    new ListTile(
+                      leading: new Icon(Icons.album),
+                      title: new Text("The Enchanted Nightingale ${exampleList[index]}"),
+                      subtitle:
+                          new Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,55 +1,201 @@
 import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:flutter_application_1/app/modules/profil_2/views/profil_2_view.dart';
 
-import '../controllers/profile_controller.dart';
+void main() => runApp(ProfileView());
 
-class ProfileView extends GetView<ProfileController> {
+class ProfileView extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //title: 'No Anggota : 1237462283',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'No Anggota : 12376283'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentIndex = 0;
+  }
+
+  void changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('No Anggota 1237462283'),
-        centerTitle: false,
+        title: Text(widget.title),
       ),
-      
-      bottomNavigationBar: BottomNavigationBar( 
+      //floatingActionButton: FloatingActionButton(
+        //onPressed: () {},
+        //child: Icon(Icons.add),
+        //backgroundColor: Colors.red,
+      //),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
         backgroundColor: Colors.blue,
-        selectedIconTheme: IconThemeData(
-          color: Colors.white
-        ),
-        //selectedIconTheme: Colors.white,
-        items: <BottomNavigationBarItem> [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('', 
-              style: TextStyle(height: 0.0))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add), 
-            title: Text('', 
-              style: TextStyle(
-                height: 0.0))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person), 
-            title: Text('', 
-              style: TextStyle(
-                height: 0.0))
-          ),
-            
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(
+                16)), //border radius doesn't work when the notch is enabled.
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              title: Text("Add")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.stacked_bar_chart,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.stacked_bar_chart,
+                color: Colors.white,
+              ),
+              title: Text("Statistik")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              title: Text("Menu")),
+           BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              title: Text("Profil"))
         ],
       ),
       body: Container(
-        color: Colors.white,
-        child: Container(
-          color: Colors.blue,
-              alignment: Alignment.topCenter,
-              height: 100,
-              width: 700,
+        child: Column(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 510,
+                )
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 150,
+                  width: 510,
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  color: Colors.blue[100],
+                  height: 305,
+                  width: 510,
+                  child: Column(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 30),
+                            color: Colors.white,
+                            height: 190,
+                            width: 450,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+
+                                //Gambar Profil
+                                Container(
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                                        width: 50,
+                                        height: 50,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(100.0),
+                                          image: DecorationImage(
+                                            image: AssetImage("assets/bg-login-2.png")
+                                          ),
+                                        ),
+                                      )
+                                    ]
+                                  ),
+                                )
+                              ]
+                            ),
+                          ),
+                        ]
+                      )
+                    ]
+                  ),
+                )
+              ],
+            )
+          ]
         ),
+        
       ),
     );
   }
