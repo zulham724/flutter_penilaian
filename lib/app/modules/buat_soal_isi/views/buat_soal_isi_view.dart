c7fc19b91e026bb1ccde12160fb59b5a55c96ea6
depa1999 <82351719+depa1999@users.noreply.github.com>
2021-04-26 19:19:52 +0700

    buat_soal_isi

--- lib/app/modules/buat_soal_isi/views/buat_soal_isi_view.dart
+++ lib/app/modules/buat_soal_isi/views/buat_soal_isi_view.dart
@@ -1,22 +1,493 @@
 import 'package:flutter/material.dart';
-
-import 'package:get/get.dart';
-
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import '../controllers/buat_soal_isi_controller.dart';
 
-class BuatSoalIsiView extends GetView<BuatSoalIsiController> {
+void main() => runApp(BuatSoalIsiView());
+
+class BuatSoalIsiView extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: ''),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    currentIndex = 0;
+  }
+
+  void changePage(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BuatSoalIsiView'),
-        centerTitle: true,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Text(
-          'BuatSoalIsiView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
+        backgroundColor: Colors.blue,
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(6)),
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: Text("Home")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              title: Text("Add")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.stacked_bar_chart,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              title: Text("Menu")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.stacked_bar_chart,
+                color: Colors.white,
+              ),
+              title: Text("Hasil")),
+           BubbleBottomBarItem(
+              backgroundColor: Colors.white,
+              icon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+              title: Text("Profil"))
+        ],
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+              child: Row(
+                children:[
+                  Text(
+                    'Buat Soal',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20
+                    ),
+                  )
+                ]
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 70),
+              child: Row(
+                children:[
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+                      child: Text(
+                        '1',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 3,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+                      child: Text(
+                        '2',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 3,
+                    width: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    width: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 7),
+                      child: Text(
+                        '3',
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+              )
+            ),
+
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 75),
+                  child: Text(
+                    'Isi',
+                    style: TextStyle(
+                      color: Colors.blue
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: Text(
+                    'Buat',
+                    style: TextStyle(
+                      color: Colors.grey
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 60),
+                  child: Text(
+                    'Selesai',
+                    style: TextStyle(
+                      color: Colors.grey
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                      child: Text(
+                        'Kelas'
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 120),
+                      child: Text(
+                        'Tipe Soal'
+                      )
+                    )
+                  ],
+                ),
+
+                Container(
+                  child: Row(  
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 100,
+                        margin: EdgeInsets.symmetric(horizontal: 20),
+                        child: DropdownButton<String>(
+                          focusColor: Colors.red,
+                          style: TextStyle(
+                            color: Colors.black
+                          ),
+                          iconEnabledColor: Colors.black,
+                          items: <String>[
+                            'aaaaa',
+                            'bbbbb',
+                            'ccccc'
+                          ]. map<DropdownMenuItem<String>>((String value) 
+                          {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value,style:TextStyle(color:Colors.black),),
+                            );
+                          }). toList(),
+                          hint: Text(
+                            "10 SMA",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                          onChanged: (String value){
+                            setState(() {
+                            });
+                          },
+                        ),
+                      ),
+
+                      Container(
+                        height: 50,
+                        width: 100,
+                        margin: EdgeInsets.symmetric(horizontal: 55),
+                        child: DropdownButton<String>(
+                          focusColor: Colors.red,
+                          style: TextStyle(
+                            color: Colors.black
+                          ),
+                          iconEnabledColor: Colors.black,
+                          items: <String>[
+                            'aaaaa',
+                            'bbbbb',
+                            'ccccc'
+                          ]. map<DropdownMenuItem<String>>((String value) 
+                          {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value,style:TextStyle(color:Colors.black),),
+                            );
+                          }). toList(),
+                          hint: Text(
+                            "Uraian",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                          onChanged: (String value){
+                            setState(() {
+                            });
+                          },
+                        ),
+                      ),    
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(
+                        'Kompetensi Dasar'
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(  
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 200,
+                        child: DropdownButton<String>(
+                          focusColor: Colors.red,
+                          style: TextStyle(
+                            color: Colors.red
+                          ),
+                          iconEnabledColor: Colors.black,
+                          items: <String>[
+                            'aaaaa',
+                            'bbbbb',
+                            'ccccc'
+                          ]. map<DropdownMenuItem<String>>((String value) 
+                          {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value,style:TextStyle(color:Colors.black),),
+                            );
+                          }). toList(),
+                          hint: Text(
+                            "Kompetensi",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                          onChanged: (String value){
+                            setState(() {
+                            });
+                          },
+                        ),
+                      ),   
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(
+                        'Kompetensi'
+                      ),
+                    ),
+                  ],
+                ),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    //maxLines: 8,
+                    decoration: 
+                    InputDecoration(
+                      hintText: "Kompetensi Dasar",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 2)
+                      )     
+                    ),
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(
+                        'Materi'
+                      ),
+                    ),
+                  ],
+                ),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    //maxLines: 8,
+                    decoration: 
+                    InputDecoration(
+                      hintText: "Kejujuran",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2)
+                      )     
+                    ),
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                      child: Text(
+                        'Indikator'
+                      ),
+                    ),
+                  ],
+                ),
+
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  child: TextField(
+                    //maxLines: 8,
+                    decoration: 
+                    InputDecoration(
+                      hintText: "Indikator",
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey, width: 2)
+                      )     
+                    ),
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 250, vertical: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                              ),
+                              color: Colors.blue,
+                              onPressed: (){},
+                              child: Text(
+                                'Lanjut',
+                                style: TextStyle(
+                                  color: Colors.white
+                                ),
+                              ),
+                            ),
+                          ]
+                        )
+                    )  
+                  ],
+                )
+              ],
+            ),    
+          ],
+        )
       ),
     );
   }
